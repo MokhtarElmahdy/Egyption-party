d85b002054497b90afde9f959f58902b69ecf4b5
MokhtarElmahdy <MOKHTAR.mohamed754@ci.menofia.edu.eg>
2022-10-21 15:13:36 +0200

    first init for app

---
+++ js/main.js
@@ -0,0 +1,57 @@
+$(function(){
+    $(".show-navigation").click(function(){
+        $(".drawer-navigation").animate(
+            {
+                width : "250px",
+                padding: '1rem'
+            }
+        );
+    });
+    $(".close-btn").click(function(){
+        $(".drawer-navigation").animate(
+            {
+                width : "0px",
+                padding: '0px'
+            }
+        );
+    });
+
+$(".navbar-link").click(function(){
+    let ref = $(this).attr('href');
+    console.log($(ref).offset().top);
+    $('html,body').animate({
+        scrollTop: `${$(ref).offset().top}px`,
+    },1000)
+})
+
+$(".accordion-title").click(function(){
+    $(this).parent().siblings().children(".accordion-content").slideUp(1000);
+    $(this).next().slideToggle(1000)
+})
+
+function countDown (){
+    let now = new Date();
+    let partyDate = new Date(2022,12,28);
+    let waitingTime = partyDate.getTime() - now.getTime();
+    let seconds = Math.floor(waitingTime / 1000);
+    let minutes = Math.floor(seconds / 60);
+    let hours = Math.floor(minutes / 60);
+    let days = Math.floor(hours / 24 - 30);
+    seconds = seconds % 60
+    minutes = minutes % 60
+    hours = hours % 24
+
+$("#Days").text(days + " d")
+$("#Hours").text(hours + " h")
+$("#Minutes").text(minutes + " m")
+$("#Seconds").text(seconds + " s")
+
+setTimeout(countDown,1000)
+
+}
+countDown()
+$("#message").keyup(function(){
+    let inputLen = $(this).val().length;
+    $("#remain span").text(100 - inputLen)
+})
+})
